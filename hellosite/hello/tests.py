6e64f9dba5d9fd22946a733971c7d01655e6cf03
vikhotin <vikhotin@yandex.ru>
2017-11-14 20:59:27 +0300

    Tests added

--- hellosite/hello/tests.py
+++ hellosite/hello/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+
+class IndexViewTests(SimpleTestCase):
+    def test_request_home_page(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Hello, world!', status_code=200)
+
+    def test_request_404_page(self):
+        response = self.client.get('/admin')
+        self.assertEqual(response.status_code, 404)
